39ee6af6471ee81446f0f1b4c41b392a2b956e6e
Raj Rao <rao.r@salesforce.com>
2018-04-26 06:48:37 -0700

    Fixed autorotate to NO by default

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Login/SFLoginViewController.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Login/SFLoginViewController.m
@@ -238,11 +238,11 @@ SFSDK_USE_DEPRECATED_END
 }
 
 - (BOOL)shouldAutorotate {
-    return YES;
+    return NO;
 }
 
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    return UIInterfaceOrientationMaskAll;
+    return UIInterfaceOrientationMaskPortrait;
 }
 
 - (void)layoutWebView {
